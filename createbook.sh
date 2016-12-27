1fd5eb75a06dbbeb9bf63d4534ec1bb68b4f507b
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2016-12-27 10:47:55 +0100

    Add gallery script

---
+++ createbook.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+export PATH="$PATH:$PWD/bin"
+
+prefix="$(sed -e '/galleryimage/,// d' content/book.html)"
+suffix="$(sed -e '0,/\/gallery/ d' content/book.html)"
+images=()
+
+for image in static/images/gallery/*; do
+  case "$image" in
+    *.thumbnail.*|*.txt)
+      continue
+      ;;
+  esac
+
+  caption="${image%.*}.txt"
+  copyright="${image%.*}.copyright.txt"
+  thumbnail="${image%.*}.thumbnail.${image##*.}"
+  size="$(identify -ping -format "%wx%h" "$image")"
+
+  if ! [[ -e "$copyright" ]]; then
+    copyright="${image%/*}/copyright.txt"
+  fi
+
+  if ! [[ "$image" -ot "$thumbnail" ]]; then
+    convert -quiet "$image" -thumbnail 250x250 +repage -unsharp 0x.5 -quality 80 "$thumbnail"
+  fi
+
+  if [[ -e "$copyright" ]]; then
+    copyright="$(cat "$copyright")"
+  else
+    copyright=
+  fi
+  if [[ -e "$caption" ]]; then
+    caption="$(cat "$caption")"
+  else
+    caption=
+  fi
+  image="/${image#static/}"
+  thumbnail="/${thumbnail#static/}"
+  images+=("{{% galleryimage file=\"$image\" size=\"$size\" thumbnail=\"$thumbnail\" caption=\"$caption\" copyrightHolder=\"$copyright\" %}}")
+done
+
+exec >content/book.html
+echo "$prefix"
+for line in "${images[@]}"; do
+  echo "$line"
+done
+echo "$suffix"
