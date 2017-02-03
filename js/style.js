2797d5676c599c00a2b7c25f2271e4b0932c4349
Elie Lombard <ellomb@alphonse.lan>
2017-02-03 18:46:39 +0100

    rebuilding site ven. févr.  3 18:46:39 CET 2017

--- js/style.js
+++ js/style.js
@@ -2,7 +2,13 @@
   document.addEventListener('scroll', onScroll)
   onScroll()
 
+  var timeout = null
   function onScroll(){
+    if(!timeout) timeout = setTimeout(setScrollTop, 10)
+  }
+
+  function setScrollTop(){
+    timeout = null
     if(document.documentElement.scrollTop > 0){
       document.documentElement.classList.remove('scroll-top')
     } else {
